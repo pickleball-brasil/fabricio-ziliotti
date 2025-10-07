f4672b44a25f55f3a3be6b8af5c3262feaaa51cb
Fabricio laptop <fabricio.ziliotti@flowls.com>
2025-10-07 10:54:44 -0300

    Update GitHub Pages URLs for pickleball-brasil organization

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/fabricioziliottipickleball/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/fabricio-ziliotti/' : '/',
   build: {
     assetsDir: 'assets',
     rollupOptions: {
